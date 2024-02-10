fd322b043a02c96d1abbb6efd1170f231b889227
kyungho Park <99066633+pkh0308@users.noreply.github.com>
2024-02-10 20:41:45 +0900

    레벨3 프레임 개선 / 레벨 2 시퀀스 테스트

---
+++ Source/P2J/PKH/Trigger/LockerDrawTrigger.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PKH/Trigger/LockerDrawTrigger.h"
+#include "JYJ/PlayerZeroCharacter.h"
+
+void ALockerDrawTrigger::OnPlayerOverlap( UPrimitiveComponent* OverlappedComponent , AActor* OtherActor , UPrimitiveComponent* OtherComp , int32 OtherBodyIndex , bool bFromSweep , const FHitResult& SweepResult )
+{
+	Super::OnPlayerOverlap( OverlappedComponent , OtherActor , OtherComp , OtherBodyIndex , bFromSweep , SweepResult );
+
+	if (IsTriggered)
+	{
+		return;
+	}
+
+	if (false == OtherActor->IsA<APlayerZeroCharacter>())
+	{
+		return;
+	}
+
+	IsTriggered = true;
+	PlaySequence();
+}
